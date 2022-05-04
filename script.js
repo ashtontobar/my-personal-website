9c1ef73ce494f003e317eb320ad48c18849766a3
Ashton Tobar <+>
2022-05-04 16:42:16 -0500

    About page

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+// const menu = document.querySelector('#mobile-menu');
+// const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+// menu.addEventListener('click', function() {
+//     menu.classList.toggle('is-active');
+//     menuLinks.classList.toggle('active');
+// })
+
+const ash = document.querySelector('.navbar__logo');
+
+
+ash.addEventListener('click', function()) {
+    ash.alert(Hi!);
 })
